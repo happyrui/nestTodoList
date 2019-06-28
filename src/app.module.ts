d777610b657381f74c19cb44e6c4f88a3c2e4bf2
happyrui <zr1450995198@163.com>
2019-06-28 18:32:53 +0800

    feat:merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,6 @@ import { TodoController } from './todo/todo.controller';
 import { Connection } from 'typeorm';
 import { TodoModule } from './todo/todo.module';
 import { LoggerMiddleware } from './config';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
 
 // @Module() 装饰器提供了元数据，Nest 用它来组织应用程序结构
 @Module({
@@ -16,8 +14,8 @@ import { UsersController } from './users/users.controller';
     TypeOrmModule.forRoot(),
     TodoModule
   ],
-  controllers: [ CatsController, TodoController, UsersController],
-  providers: [ CatService, TodoService, UsersService],
+  controllers: [ CatsController, TodoController],
+  providers: [ CatService, TodoService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
